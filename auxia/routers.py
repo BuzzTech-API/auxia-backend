4c9ae8a416cfa7b7e9e3acb7e385d4ae16a2e414
lima2206 <vitor.lima2206@gmail.com>
2025-03-18 09:19:32 -0300

    feat: criar a rota post para salvar uma resposta no banco de dados

--- auxia/routers.py
+++ auxia/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from auxia.controllers.ai import router as ai_router
+from auxia.controllers.ai import router as ai
+from auxia.controllers.answer import router as answer
 
 api_router = APIRouter()
 
-api_router.include_router(ai_router)
+api_router.include_router(ai)
+api_router.include_router(answer)
