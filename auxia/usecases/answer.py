704747cd349ed3212d4103bd8870c2e6629c80c1
lima2206 <vitor.lima2206@gmail.com>
2025-03-28 13:53:49 -0300

    refactor: remover comentários

--- auxia/usecases/answer.py
+++ auxia/usecases/answer.py
@@ -18,15 +18,6 @@ class AnswerUsecase:
         self.collection = self.database.get_collection("answer")
     
     async def saveAnswer(self, answer: AnswerRequest):
-        # answer_data = AnswerModel(**answer.model_dump())
-        
-        # # Por algum motivo no windows o driver do mongo pro python não consegue trabalhar com o uuid padrão do mongo
-        # if hasattr(answer_data, "id") and isinstance(answer_data.id, uuid.UUID):
-        #     answer_data.id = Binary.from_uuid(answer_data.id, uuid_representation=UuidRepresentation.STANDARD)
-        
-        # await self.collection.insert_one(answer_data.model_dump())
-        # return 
-
         try:
             answer_data = AnswerModel(**answer.model_dump())
             
