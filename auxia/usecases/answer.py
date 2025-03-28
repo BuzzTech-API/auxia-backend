2ca50b1fbe367214a151f728cccafa8aeca32c2d
lima2206 <vitor.lima2206@gmail.com>
2025-03-28 13:52:04 -0300

    feat: adicionar as exceções do salvamento no banco de dados e os testes

--- auxia/usecases/answer.py
+++ auxia/usecases/answer.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+from http.client import HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from bson import Binary, UuidRepresentation
 import uuid
+
+from pydantic import ValidationError
 
 from auxia.db.mongo import db_client
 from auxia.models.answer import AnswerModel
@@ -15,14 +18,35 @@ class AnswerUsecase:
         self.collection = self.database.get_collection("answer")
     
     async def saveAnswer(self, answer: AnswerRequest):
-        answer_data = AnswerModel(**answer.model_dump())
-        # Por algum motivo no windows o driver do mongo pro python não consegue trabalhar com o uuid padrão do mongo
+        # answer_data = AnswerModel(**answer.model_dump())
         
-        if hasattr(answer_data, "id") and isinstance(answer_data.id, uuid.UUID):
-            answer_data.id = Binary.from_uuid(answer_data.id, uuid_representation=UuidRepresentation.STANDARD)
+        # # Por algum motivo no windows o driver do mongo pro python não consegue trabalhar com o uuid padrão do mongo
+        # if hasattr(answer_data, "id") and isinstance(answer_data.id, uuid.UUID):
+        #     answer_data.id = Binary.from_uuid(answer_data.id, uuid_representation=UuidRepresentation.STANDARD)
         
-        await self.collection.insert_one(answer_data.model_dump())
-        return 
+        # await self.collection.insert_one(answer_data.model_dump())
+        # return 
+
+        try:
+            answer_data = AnswerModel(**answer.model_dump())
+            
+            # Por algum motivo no windows o driver do mongo pro python não consegue trabalhar com o uuid padrão do mongo
+            if hasattr(answer_data, "id") and isinstance(answer_data.id, uuid.UUID):
+                answer_data.id = Binary.from_uuid(answer_data.id, uuid_representation=UuidRepresentation.STANDARD)
+            
+            await self.collection.insert_one(answer_data.model_dump())
+            return {"message": "Resposta Salva com sucesso!"}
+        
+        except ValidationError as e:
+            raise HTTPException(
+                status_code=422,
+                detail=e.errors()
+            )
+        except Exception as e:
+            raise HTTPException(
+                status_code=500,
+                detail=str(e)
+            )
 
 
 answer_usecase = AnswerUsecase()
--- tests/controllers/test_answer.py
+++ tests/controllers/test_answer.py
@@ -9,3 +9,32 @@ async def test_controller_generate_should_return_success(
 ):
     response = await client.post(answers_url, json=answer_data())
     assert response.status_code == status.HTTP_201_CREATED
+    
+async def test_controller_generate_should_return_unprocesseble(
+    client,
+    answers_url,
+):
+    response = await client.post(answers_url, json={
+        "usr_id": 202,
+        "ans_prompt": "",
+        "ans_llm_answer": "The chemical formula for water is H2O.",
+        "ans_llm_model": "GPT-4",
+        "ans_evaluation_standard_1_justify": "The answer is scientifically accurate.",
+        "ans_evaluation_standard_1_pontuation": 5,
+        "ans_evaluation_standard_2_justify": "The response is concise and clear.",
+        "ans_evaluation_standard_2_pontuation": 5,
+        "ans_evaluation_standard_3_justify": "The answer does not provide additional context, but it is not necessary.",
+        "ans_evaluation_standard_3_pontuation": 4,
+        "ans_evaluation_standard_4_justify": "The response is well-structured.",
+        "ans_evaluation_standard_4_pontuation": 5,
+        "ans_evaluation_standard_5_justify": "The answer uses simple language.",
+        "ans_evaluation_standard_5_pontuation": 5,
+        "ans_evaluation_standard_6_justify": "The response is consistent with established chemical knowledge.",
+        "ans_evaluation_standard_6_pontuation": 5,
+        "ans_evaluation_standard_7_justify": "The answer is delivered quickly.",
+        "ans_evaluation_standard_7_pontuation": 5,
+        "ans_prefered_answer": True,
+        "ans_prefered_answer_justify": "This answer is accurate and easy to understand.",  
+    })
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
